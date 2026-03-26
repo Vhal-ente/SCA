ee69a431754086a8c43b023146192fe55d1a46e8
Vhal-ente <valentinenwobi9@gmail.com>
2026-03-26 17:33:59 +0100

    commit message for deployment on vercel 1

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/SCA/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
